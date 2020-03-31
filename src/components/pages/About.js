ed8315d19b195f2552ceb9fac14123bdfec04338
Ally Brannon <allison.brannon@gmail.com>
2020-03-31 08:49:47 -0400

    commit files for to do list following video

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the To Do List app. It is part of the:
+        <a href="https://www.youtube.com/watch?v=sBws8MSXN7A">
+          React JS Crash Course
+        </a>
+      </p>
+    </React.Fragment>
+  );
+}
